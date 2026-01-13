d64e21f1703bc3b2f6b30896cd4d0ffb4c09319e
lsb <lsb5116@naver.com>
2026-01-13 16:05:53 +0900

    design mockup

---
+++ src/test/java/com/lsb/kkirikkiri/KkirikkiriApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lsb.kkirikkiri;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KkirikkiriApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
