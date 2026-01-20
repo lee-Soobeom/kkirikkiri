ad5170dafe5a71cb6fe3a726d02b637117b81ba2
이지안 <ljh5898@hanmail.net>
2026-01-20 14:38:10 +0900

    write.html, write.scss

--- src/main/java/com/lsb/kkirikkiri/controllers/ArticleController.java
+++ src/main/java/com/lsb/kkirikkiri/controllers/ArticleController.java
@@ -1,4 +1,19 @@
 package com.lsb.kkirikkiri.controllers;
 
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.SessionAttribute;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping(value = "/article")
 public class ArticleController {
+    @RequestMapping(value = "/",
+            method = RequestMethod.GET,
+            produces = MediaType.TEXT_HTML_VALUE)
+    public String getArticle() {
+        return "article/write";
+    }
 }
