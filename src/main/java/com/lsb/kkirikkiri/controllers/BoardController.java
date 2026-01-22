11b0a01362483e2c8455e2a81c92356aa268d466
이지안 <ljh5898@hanmail.net>
2026-01-22 12:07:49 +0900

    article.scss, boardController, index.scss, list.html, list.scss, login.html, login.scss, register.scss, write.scss 수정 및 추가 unsigned.scss 삭제.

---
+++ src/main/java/com/lsb/kkirikkiri/controllers/BoardController.java
@@ -0,0 +1,19 @@
+package com.lsb.kkirikkiri.controllers;
+
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping(value = "/board")
+public class BoardController {
+    @RequestMapping(value = "/list",
+    method = RequestMethod.GET,
+    produces = MediaType.TEXT_HTML_VALUE)
+    public ModelAndView getBoard(ModelAndView modelAndView) {
+        modelAndView.setViewName("board/list");
+        return modelAndView;
+    }
+}
